819239e1f79b439f9f100b511e82a339346fe65e
vs0uz4 <vitor.rodrigues@gmail.com>
2018-11-19 01:43:54 -0200

    Implemented login and user creation methods. And changed the view index to adding the links to their methods.

---
+++ account.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const route = express.Router()
+
+const init = connection => {
+  route.get('/sign-in', (req, res) => {
+    res.render('account/sign-in')
+  })
+
+  route.post('/sign-in', async (req, res) => {
+    const {
+      email,
+      passwd
+    } = req.body
+
+    const [rows, fields] = await connection.execute('SELECT * FROM users WHERE email = ?', [email])
+    if (rows.length !== 0 && rows[0].passwd === passwd) {
+      const userDb = rows[0]
+      const user = {
+        id: userDb.id,
+        name: userDb.name,
+        role: userDb.role
+      }
+      req.session.user = user
+
+      res.redirect('/')
+    } else {
+      res.render('account/sign-in', {
+        error: 'Usuário e/ou senha inválidos.',
+        email: email,
+        passwd: passwd
+      })
+    }
+  })
+
+  route.get('/sign-out', (req, res) => {
+    req.session.destroy(err => {
+      if (err) {
+        console.log(`Error Destroying Session\nError: ${err.code}\nMessage ${err.message}`)
+      }
+
+      res.redirect('/')
+    })
+  })
+
+  route.get('/new-account', (req, res) => {
+    res.render('account/new')
+  })
+
+  route.post('/new-account', async (req, res) => {
+    const {
+      name,
+      email,
+      passwd
+    } = req.body
+
+    const [rows, fields] = await connection.execute('SELECT * FROM users WHERE email = ?', [email])
+    if (rows.length === 0) {
+      await connection.execute('INSERT INTO users (name, email, passwd, role) VALUES (?, ?, ?, ?)', [
+        name,
+        email,
+        passwd,
+        'user'
+      ])
+      res.redirect('/')
+    } else {
+      res.render('account/new', {
+        error: 'Usuário já existente.',
+        name: name,
+        email: email,
+        passwd: passwd
+      })
+    }
+  })
+
+  return route
+}
+
+module.exports = init
--- index.js
+++ index.js
@@ -1,19 +1,55 @@
 require('dotenv').config()
 const express = require('express')
+const session = require('express-session')
+const mysql = require('mysql2/promise')
+const bodyParser = require('body-parser')
+
+const account = require('./account')
 
 // Get Enviroment Variables
 const port = process.env.APP_PORT || 3000
 const appName = process.env.APP_NAME || 'FutibaClub'
+const dbHost = process.env.DB_HOST || 'localhost'
+const dbPort = process.env.DB_PORT || 3306
+const dbDatabase = process.env.DB_DATABASE || 'futibaclub'
+const dbUsername = process.env.DB_USERNAME || 'root'
+const dbPassword = process.env.DB_PASSWORD || 'secret'
+const sessionSecret = process.env.SESSION_SECRET || 'secret'
 
 const app = express()
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(session({
+  secret: sessionSecret,
+  saveUninitialized: true,
+  resave: true
+}))
 app.set('view engine', 'ejs')
 
 const init = async () => {
   try {
-    app.get('/', (req, res) => {
+    const connection = await mysql.createConnection({
+      host: dbHost,
+      port: dbPort,
+      user: dbUsername,
+      password: dbPassword,
+      database: dbDatabase
+    })
+
+    app.use((req, res, next) => {
+      if (req.session.user) {
+        res.locals.user = req.session.user
+      }
+      next()
+    })
+
+    app.get('/', async (req, res) => {
       res.render('home')
     })
+
+    app.use(account(connection))
 
     app.listen(port, error => {
       if (error) {
@@ -26,4 +62,5 @@ const init = async () => {
     console.log(`Error occurred in Initialize App\nError: ${error.code}\nMessage:${error.message}`)
   }
 }
+
 init()
