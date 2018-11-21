579c0f25e16c646e9fb7baf349efac26560ae9ca
vs0uz4 <vitor.rodrigues@gmail.com>
2018-11-21 02:06:48 -0200

    Updated middleware for check authorization of the groups routes and setting url parameter in locals variables

--- middleware.js
+++ middleware.js
@@ -1,10 +1,16 @@
 module.exports = (req, res, next) => {
   if (req.url.includes('/admin') && (!req.session.user || req.session.user.role === 'user')) {
+    res.locals.url = req.url
+    res.redirect('/')
+  } else if (req.url.includes('/groups') && (!req.session.user)) {
+    res.locals.url = req.url
     res.redirect('/')
   } else if (req.session.user) {
+    res.locals.url = req.url
     res.locals.user = req.session.user
     next()
   } else {
+    res.locals.url = req.url
     next()
   }
 }
