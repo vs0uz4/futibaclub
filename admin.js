63b77bce1d1f639a52615ff97e505cb951acdd39
vs0uz4 <vitor.rodrigues@gmail.com>
2018-11-28 22:44:50 -0200

    Implemented update and invalidated of cache on redis

--- admin.js
+++ admin.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const route = express.Router()
+const encRedisClient = require('./cache/setup')
 
 const init = connection => {
   route.get('/', (req, res) => {
@@ -77,6 +78,26 @@ const init = connection => {
       })
 
       await Promise.all(batch)
+
+      const [groups] = await connection.execute('SELECT groups.id, groups.name, SUM(guessings.score) as score FROM groups LEFT JOIN guessings ON guessings.group_id = groups.id GROUP BY groups.id ORDER BY score DESC')
+      await encRedisClient.setexAsync('groupsRanking', 84600, groups, (err) => {
+        if (err) {
+          console.log(err.message)
+        }
+      })
+
+      const [users] = await connection.execute('SELECT users.id, users.name, SUM(guessings.score) as score FROM users LEFT JOIN guessings on guessings.user_id = users.id GROUP BY users.id ORDER BY score DESC')
+      await encRedisClient.setexAsync('usersRanking', 84600, users, (err) => {
+        if (err) {
+          console.log(err.message)
+        }
+      })
+
+      await encRedisClient.setAsync('invalidatedCache', true, (err) => {
+        if (err) {
+          console.log(err.message)
+        }
+      })
     }
 
     res.redirect('/admin/games')
