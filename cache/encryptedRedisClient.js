4ab19f347c822ed7dfec801f7f52a9c442ad02dc
vs0uz4 <vitor.rodrigues@gmail.com>
2018-11-28 22:40:51 -0200

    Refactored class and implement new async methods for get, set and setex on redis client

--- cache/encryptedRedisClient.js
+++ cache/encryptedRedisClient.js
@@ -12,7 +12,31 @@ class EncryptedRedisClient {
         cb(err)
       }
 
-      cb(null, this.safe.decrypt(reply))
+      if (reply) {
+        cb(null, this.safe.decrypt(reply))
+      } else {
+        cb(null, [])
+      }
+    })
+  }
+
+  getAsync (key, cb) {
+    return new Promise((resolve, reject) => {
+      try {
+        this.redisClient.get(key, (err, reply) => {
+          if (err) {
+            reject(cb(err))
+          }
+
+          if (reply) {
+            resolve(cb(null, this.safe.decrypt(reply)))
+          } else {
+            resolve(cb(null, []))
+          }
+        })
+      } catch (exception) {
+        reject(exception)
+      }
     })
   }
 
@@ -21,9 +45,35 @@ class EncryptedRedisClient {
     this.redisClient.set(key, encValue, cb)
   }
 
+  setAsync (key, value, cb) {
+    return new Promise((resolve, reject) => {
+      try {
+        const encValue = this.safe.encrypt(value)
+        const reply = this.redisClient.set(key, encValue, cb)
+
+        resolve(reply)
+      } catch (exception) {
+        reject(exception)
+      }
+    })
+  }
+
   setex (key, seconds, value, cb) {
     const encValue = this.safe.encrypt(value)
     this.redisClient.setex(key, seconds, encValue, cb)
+  }
+
+  setexAsync (key, seconds, value, cb) {
+    return new Promise((resolve, reject) => {
+      try {
+        const encValue = this.safe.encrypt(value)
+        const reply = this.redisClient.setex(key, seconds, encValue, cb)
+
+        resolve(reply)
+      } catch (exception) {
+        reject(exception)
+      }
+    })
   }
 }
 
