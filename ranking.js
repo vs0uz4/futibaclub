9695487b247740f992eb85ecc6ce2a928b889a72
vs0uz4 <vitor.rodrigues@gmail.com>
2018-11-28 22:44:00 -0200

    Implemented ranking module for calculate scores of groups and users and update cache on redis

--- ranking.js
+++ ranking.js
@@ -1,15 +1,77 @@
 const express = require('express')
 const route = express.Router()
+const encRedisClient = require('./cache/setup')
 
 const init = connection => {
-  route.get('/', async (req, res) => {
-    const [groupsRanking] = await connection.execute('SELECT groups.id, groups.name, SUM(guessings.score) as score FROM groups LEFT JOIN guessings ON guessings.group_id = groups.id GROUP BY groups.id ORDER BY score DESC')
-    const [usersRanking] = await connection.execute('SELECT users.id, users.name, SUM(guessings.score) as score FROM users LEFT JOIN guessings on guessings.user_id = users.id GROUP BY users.id ORDER BY score DESC')
+  let classification = null
 
-    res.render('ranking', {
-      groupsRanking,
-      usersRanking
+  route.get('/', async (req, res) => {
+    await encRedisClient.getAsync('invalidatedCache', async (err, reply) => {
+      if (err) {
+        console.log(err.message)
+      }
+
+      if (reply) {
+        classification = null
+      }
     })
+
+    if (!classification) {
+      const groupsRanking = await encRedisClient.getAsync('groupsRanking', async (err, reply) => {
+        if (err) {
+          console.log(err.message)
+        } else {
+          if (reply.length > 0) {
+            return reply
+          } else {
+            const [groups] = await connection.execute('SELECT groups.id, groups.name, SUM(guessings.score) as score FROM groups LEFT JOIN guessings ON guessings.group_id = groups.id GROUP BY groups.id ORDER BY score DESC')
+
+            await encRedisClient.setexAsync('groupsRanking', 84600, groups, (err) => {
+              if (err) {
+                console.log(err.message)
+              }
+            })
+
+            return groups
+          }
+        }
+      })
+
+      const usersRanking = await encRedisClient.getAsync('usersRanking', async (err, reply) => {
+        if (err) {
+          console.log(err.message)
+        } else {
+          if (reply.length > 0) {
+            return reply
+          } else {
+            const [users] = await connection.execute('SELECT users.id, users.name, SUM(guessings.score) as score FROM users LEFT JOIN guessings on guessings.user_id = users.id GROUP BY users.id ORDER BY score DESC')
+
+            await encRedisClient.setexAsync('usersRanking', 84600, users, (err) => {
+              if (err) {
+                console.log(err.message)
+              }
+            })
+
+            return users
+          }
+        }
+      })
+
+      await encRedisClient.setAsync('invalidatedCache', false, (err) => {
+        if (err) {
+          console.log(err.message)
+        }
+      })
+
+      classification = {
+        groupsRanking,
+        usersRanking
+      }
+
+      console.log(classification)
+    }
+
+    res.render('ranking', classification)
   })
 
   return route
