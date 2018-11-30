607b6743a5cf62dbbab578e9d95f7f3bd7b73490
vs0uz4 <vitor.rodrigues@gmail.com>
2018-11-30 13:31:28 -0200

    Implemented contact section with preview for mail messages

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mysql = require('mysql2/promise')
 const bodyParser = require('body-parser')
 
 const middleware = require('./middleware')
+const mailer = require('./utils/mailer')
 const admin = require('./admin')
 const account = require('./account')
 const groups = require('./groups')
@@ -19,6 +20,8 @@ const dbDatabase = process.env.DB_DATABASE || 'futibaclub'
 const dbUsername = process.env.DB_USERNAME || 'root'
 const dbPassword = process.env.DB_PASSWORD || 'secret'
 const sessionSecret = process.env.SESSION_SECRET || 'secret'
+const etherealUser = process.env.ETHEREAL_USER || 'user'
+const etherealPasswd = process.env.ETHEREAL_PASSWD || 'secret'
 
 const app = express()
 app.use(express.static('public'))
@@ -45,6 +48,31 @@ const init = async () => {
 
     app.get('/', async (req, res) => {
       res.render('home')
+    })
+
+    app.post('/', async (req, res) => {
+      const account = {
+        user: etherealUser,
+        pass: etherealPasswd
+      }
+
+      const subjects = {
+        1: 'Reclamações',
+        2: 'Sugestões',
+        3: 'Informações',
+        4: 'Outros'
+      }
+
+      const mailerOptions = {
+        name: req.body.name,
+        email: req.body.email,
+        subject: subjects[req.body.subject],
+        message: req.body.message
+      }
+
+      const reply = await mailer(account, mailerOptions)
+
+      res.render('home', reply)
     })
 
     app.use(account(connection))
