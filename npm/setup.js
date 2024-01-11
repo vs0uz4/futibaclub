4034fd491d5dbd3e12592dae8cb925d0515dcdac
vs0uz4 <vitor.rodrigues@gmail.com>
2024-01-11 20:43:11 -0300

    remove hard-code credentials

--- npm/setup.js
+++ npm/setup.js
@@ -4,6 +4,8 @@ const redis = require('redis')
 const config = require('./config')
 const Safe = require('../utils/safe')
 const EncryptedRedisClient = require('./encryptedRedisClient')
+let key = process.env.ENCRYPTED_KEY
+let iv = process.env.ENCRYPTED_IV
 
 const client = redis.createClient(config)
 
@@ -15,6 +17,6 @@ client.on('error', (err) => {
   console.log('Something went wrong ' + err)
 })
 
-const encClient = new EncryptedRedisClient(client, new Safe('3zTvzr3p67VC61jmV54rIYu1545x4TlY', '60iP0h6vJoEa'))
+const encClient = new EncryptedRedisClient(client, new Safe(this.key, this.iv))
 
 module.exports = encClient
